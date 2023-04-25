5aa280bc0f4facd4e71cb75ce85490e27aad96fc
Palida580 <124670810+Palida580@users.noreply.github.com>
2023-04-25 15:53:29 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint ={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "SiamParagon",
+          "type": "Special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.53494849027572,
+            13.746682627624779
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "BenjakitiPark",
+          "type": "Nature",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.55854521703145,
+            13.730587047742617
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "WatArun",
+          "type": "Culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.48889103676606,
+            13.743660868720355
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "VachirabenjatasPark",
+          "type": "Nature",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.55353276352264,
+            13.81567721161096
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "HarajukuDessert",
+          "type": "Special",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.57201747802304,
+            13.734646080723948
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "MaharajPier",
+          "type": "Special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.48869949027949,
+            13.754595370242242
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "AsiatiqueTheRiverfront",
+          "type": "Special",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.50413749028058,
+            13.705023548727112
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "SafariWorld",
+          "type": "Nature",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.70409301352134,
+            13.866698199003139
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "KingPowerMahanakhon",
+          "type": "Special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.52877294378499,
+            13.723661627491268
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ChinaTown",
+          "type": "Special",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.50936921703152,
+            13.740700207827757
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
